3540b94b31925f76f36cd9c6ffbc271354b3293d
sang970608 <sang970608@naver.com>
2020-09-15 19:14:14 +0900

    Updates

---
+++ precache-manifest.b591261b0fbe938125662551e9bbac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7878e7ffb060b205740ccad97265e25c",
+    "url": "/bgm/index.html"
+  },
+  {
+    "revision": "284ee6fb82a8a41665ef",
+    "url": "/bgm/static/css/main.3ba60e25.chunk.css"
+  },
+  {
+    "revision": "1a7355491432350b1a8e",
+    "url": "/bgm/static/js/2.aa417683.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bgm/static/js/2.aa417683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284ee6fb82a8a41665ef",
+    "url": "/bgm/static/js/main.15abfe7b.chunk.js"
+  },
+  {
+    "revision": "c2825d20752165c3ccf1",
+    "url": "/bgm/static/js/runtime-main.04f190d6.js"
+  }
+]);
